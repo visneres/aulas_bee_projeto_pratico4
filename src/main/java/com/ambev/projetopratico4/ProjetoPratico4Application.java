33a1bd8bd143a2e8f38cce4af51bd6a1449fa9f7
Emerson Fonseca <idd_emefonseca@uolinc.com>
2023-11-15 16:14:40 -0300

    Inicial

---
+++ src/main/java/com/ambev/projetopratico4/ProjetoPratico4Application.java
@@ -0,0 +1,13 @@
+package com.ambev.projetopratico4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPratico4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPratico4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambev/projetopratico4/ProjetoPratico4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambev.projetopratico4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPratico4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
